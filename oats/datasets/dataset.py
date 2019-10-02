c8ef72a87cd02e44227571e00f0a1557c1e02ed2
irbraun <irbraun@iastate.edu>
2019-10-02 16:56:56 -0500

    removing the index reset from filter functions

--- oats/datasets/dataset.py
+++ oats/datasets/dataset.py
@@ -19,6 +19,7 @@ import sys
 import glob
 
 
+
 from oats.datasets.gene import Gene
 from oats.nlp.preprocess import concatenate_descriptions
 from oats.nlp.preprocess import concatenate_with_bar_delim
@@ -53,8 +54,6 @@ class Dataset:
 
 
 
-
-
 	def add_data(self, df_newlines):
 		"""
 		Add a dataframe containing information to this dataset. The dataframe provided has to 
@@ -63,7 +62,7 @@ class Dataset:
 		still unique and useful as matrix indices (integers from 0 to n) after any data is added
 		to the dataset.
 		Args:
-		    df_newlines (pandas.DataFrame): The dataframe containing rows to be added.
+			df_newlines (pandas.DataFrame): The dataframe containing rows to be added.
 		"""
 		df_newlines = df_newlines[self.col_names_without_id]
 		df_newlines.loc[:,"id"] = None
@@ -75,57 +74,63 @@ class Dataset:
 
 
 
+
+
+
+
 	def filter_random_k(self, k, seed):
 		"""Remove all but k randomly sampled points from the dataset.
 		Args:
-		    k (int): The number of datapoints or rows to retain.
-		    seed (int): Seed value for reproducibility of random process.
+			k (int): The number of datapoints or rows to retain.
+			seed (int): Seed value for reproducibility of random process.
 		"""
 		self.df = self.df.sample(n=k, random_state=seed)
-		self._reset_ids()
+		#self._reset_ids()
 
+
+	def filter_by_species(self, species):
+		"""Retain only points that are for species in this list.
+		"""
+		self.df = self.df[self.df["species"].isin(species)]
+		#self._reset_ids()
 
 
 	def filter_has_description(self):
 		"""Remove points that don't have a text description.
 		"""
 		self.df = self.df[self.df["description"] != ""] 
-		self._reset_ids()
+		#self._reset_ids()
 
 
 	def filter_has_annotation(self):
 		"""Remove points that don't have atleast one ontology term annotation.
 		"""
 		self.df = self.df[self.df["term_ids"] != ""]
-		self._reset_ids()
-
+		#self._reset_ids()
 
 
 	def filter_with_ids(self, ids):
 		"""Retain only points that have IDs in the list. 
 		"""
 		self.df = self.df[self.df["id"].isin(ids)]
-		self._reset_ids()
-
-	def filter_by_species(self, species):
-		"""Retain only points that are for species in this list.
-		"""
-		self.df = self.df[self.df["species"].isin(species)]
-		self._reset_ids()
+		#self._reset_ids()
 
 
 
 
-	
 
 
 
+
+
+
+	######## Getting dictionaries that map IDs to fields in the dataset #########
 
 
 	def get_gene_dictionary(self):
 		"""Get a mapping from IDs to gene objects.
 		Returns:
-		    TYPE: Description
+			TYPE: Description
 		"""
 		gene_dict = {}
 		for row in self.df.itertuples():
@@ -136,11 +141,10 @@ class Dataset:
 		return(gene_dict)
 
 
-
 	def get_annotations_dictionary(self):
 		"""Get a mapping from IDs to lists of ontology term IDs.
 		Returns:
-		    TYPE: Description
+			TYPE: Description
 		"""
 		annotations_dict = {}
 		for row in self.df.itertuples():
@@ -150,11 +154,10 @@ class Dataset:
 		return(annotations_dict)
 
 
-
 	def get_description_dictionary(self):
 		"""Get a mapping from IDs to text descriptions.
 		Returns:
-		    TYPE: Description
+			TYPE: Description
 		"""
 		description_dict = {identifier:description for (identifier,description) in zip(self.df.id, self.df.description)}
 		return(description_dict)
@@ -165,15 +168,51 @@ class Dataset:
 
 
 
+	######### Reverse mappings (where the ID is the value) which might be useful #########
+
+
+	def get_name_to_id_dictionary(self):
+		"""
+		Get a mapping between gene names and the object ID they refer to. Note that this is 
+		guaranteed to not overwrite the values for any keys (no two names refer to different
+		IDs) provided that the collapse by all gene names method has been run on this data,
+		but this is not guaranteed if that method has not been run.
+		"""
+		name_to_id_dict = {}
+		for row in self.df.itertuples():
+			delim = "|"
+			names = row.gene_names.split(delim)
+			for name in names:
+				name_to_id_dict[name] = row.id
+		return(name_to_id_dict) 
+
+
+
+
+
+
+
+
+
+
+
+
+	######## Merging entries in the dataset based on overlaps in gene names #########
+
+
+
+
 
 	def collapse_by_first_gene_name(self):
 		"""
 		This method reorganizes the internal dataframe object so that any lines that were referring
 		to the same species and where the first value in the list of gene names is identical for 
 		the entry are merged. Text descriptions are concatenated and a union of the gene names, term
-		IDs and references are retained. This is much faster than collapsing based on all the gene
-		names because it can use the groupby function because it's looking at a single value rather
-		than an overlap in a list of values. 
+		IDs and references are retained. This is not appreciably faster than collapsing by all the 
+		gene names instead of just the first one, because that problem can be formulated as solving
+		the connected components problem before doing the group by step. So this method is included
+		not to save time but for cases where only the first gene name is unique gene identifer and 
+		the other are potentially not.
 		"""
 
 		# Create the column that can be used to group by.
@@ -194,7 +233,21 @@ class Dataset:
 
 
 
-
+	
+	@staticmethod
+	def generate_edges(row):
+		"""
+		Given a row in a dataframe that matches the column signature of the one used here, generate
+		a list of edges between the entry's ID and each of the gene names mentioned for that entry.
+		This uses a representation of the gene names that includes the species as a prefix so that
+		the same gene name used for two different species are not considered the same gene. This is 
+		treating the gene names and ID's as nodes in a graph for the purpose of collapsing the dataset
+		as a connected components problem, see the description in the function to collapse by all 
+		gene names below.
+		"""
+		names = row["gene_names"].split("|")
+		edges = [(row["id"],"{}.{}".format(row["species"],name)) for name in names]
+		return(edges)
 
 
 
@@ -202,74 +255,95 @@ class Dataset:
 
 	def collapse_by_all_gene_names(self):
 		"""
-		This method reorganizes the internal dataframe object so that any lines that were referring
-		to the same species and that have an overlap of atleast one in the gene names that were included
-		for those lines are combined. This is slow. All data is retained, so that a union of the gene 
-		names and union of ontology term annotations are found for any lines combined. Text descriptions
-		are combined as well by concatenating using the supporting methods.
+		The input is the dataframe that has the form:
+
+		ID  Names
+		1	A
+		2	A,B
+		3	B,C,D
+		4   E,F
+
+		The behavior of this function should be to add an additional column that has one unique value
+		for all the rows that, based on the assumption that all values in 'Names' are unique identifers,
+		those rows all represent the same actual gene. Note that this might not be a good assumption for 
+		some of the sources of gene synonyms in some of the datasets used, so only values that are likely
+		unique such as true accessions of gene model names should be included in the column used as the 
+		gene names column here. The result of adding a new component column would be as follows.
+
+		ID  Names    Component
+		1	A        1
+		2	A,B      1
+		3	B,C,D    1
+		4   E,F      2
+
+		Then the dataframe can be aggregated based on the values in the component column to yield the 
+		following smaller version of the dataframe, with reset ID values to reflect the componets.
+
+		ID  Names    Component
+		1	A,B,C,D  1
+		2   E,F      2
+
+		This is solved by treating the ID values and the name values as unique node names in a graph, 
+		and then given the set of edges that are specifed in that dataframe (1-A, 1-B, ..., 4-F), the
+		problem can be solved as a connected components problem.
 		"""
 
-		# Only perform this operation on slices of the data for one species at a time.
-		# This Enforces that genes with the same name across two species have to be two seperate nodes.
-		# Nodes that correspond to different species can never be merged.
-		num_new_rows = 0
-		for species in pd.unique(self.df.species):
-			print(species)
+		import networkx as nx
+		from itertools import chain
 
+		# Build the graph model of this data where nodes are gene names or IDs.
+		g = nx.Graph()
+		edges = self.df.apply(Dataset.generate_edges, axis=1)
+		edges = list(chain.from_iterable(edges.values))
+		g.add_edges_from(edges)
 
-			# (1) Create a mapping from gene name strings to row indices where that string is mentioned.
-			gene_mention_map = defaultdict(list)
-			for row in self.df.itertuples():
-				if row.species == species:
-					delim = "|"
-					gene_names = row.gene_names.split(delim)
-					for gene_name in gene_names:
-						gene_mention_map[gene_name].append(row.Index)
+		# Get the connected components of the graph, a list of lists of nodes. Each component will
+		# always have one or more ID's in it as (a) node(s), because every gene name has to be
+		# associated with atleast one ID, corresponding to the row that it was in in the input data.
+		components = nx.connected_components(g)
 
+		# Build a dictionary mapping node names (IDs and gene names) to component values.
+		# Is there a more efficient way to get a node to component mapping? There probably
+		# should be we don't need everything to be in the mapping, just one thing from each 
+		# entry, which could be just the ID values, don't need any of the gene names. This is
+		# because all node mentioned in one entry will always be in the same component. One 
+		# solution would be to sort all the lists of nodes then just take the first, if we
+		# could make sure the ID value would always be first? That might be even slower, 
+		# depends on ratio of number of entries to number of names?
+		node_to_component = {}
+		component_index = 0
+		for node_set in components:
+			for node in node_set:
+				node_to_component[node] = component_index
+			component_index = component_index+1
 
-			# (2) Create a list of sets where all indices in a given set contain synonymous genes (overlap in >=1 name used).
-			list_of_sets_of_row_indices = []
-			for gene_name in gene_mention_map.keys():
+		# Create a new column that indicates which connected component that entry maps to.
+		self.df["component"] = self.df["id"].map(node_to_component)
 
-				# Determine which existing synonymous set this gene belongs to, if any.
-				set_index_where_this_gene_belongs = -1
-				i = 0
-				for set_of_row_indices in list_of_sets_of_row_indices:
-					if len(set(gene_mention_map[gene_name]).intersection(set_of_row_indices)) > 0:
-						set_index_where_this_gene_belongs = i
-						list_of_sets_of_row_indices[i].update(gene_mention_map[gene_name])
-						break
-					i = i+1
-
-				# If this gene doesn't belong in any of those sets, start a new set with all it's corresponding row indices.
-				if set_index_where_this_gene_belongs == -1:
-					list_of_sets_of_row_indices.append(set(gene_mention_map[gene_name]))
-
-
-			# (3) Add rows which contain merged information from multiple rows where the same gene was mentioned.
-			num_new_rows = num_new_rows + len(list_of_sets_of_row_indices)
-			for set_of_row_indices in list_of_sets_of_row_indices:
-				relevant_rows = self.df.iloc[list(set_of_row_indices)]
-				description = concatenate_descriptions(*relevant_rows.description.tolist())
-				gene_names = concatenate_with_bar_delim(*relevant_rows.gene_names.tolist())
-				term_ids = concatenate_with_bar_delim(*relevant_rows.term_ids.tolist())
-				pmids = concatenate_with_bar_delim(*relevant_rows.pmid.tolist())
-				new_row = {
-					"id":None,
-					"species":species,
-					"gene_names":gene_names,
-					"description":description,
-					"term_ids":term_ids,
-					"pmid":pmids,
-				}
-				self.df.append(new_row, ignore_index=True, sort=False)
-
-
-		# Retain only the newly added rows, reset the ID values for each row that correspond to one node.
-		self.df = self.df.iloc[-num_new_rows:]
+		# Groupy by the connected component column and merge the other fields appropriately.
+		self.df = self.df.groupby("component").agg({"species": lambda x: x.values[0],
+															"gene_names": lambda x: concatenate_with_bar_delim(*x),
+															"description":lambda x: concatenate_descriptions(*x),
+															"term_ids":lambda x: concatenate_with_bar_delim(*x),
+															"pmid":lambda x: concatenate_with_bar_delim(*x)})
+		
+		# Reset the ID values in the dataset to reflect this change.
+		self.df["id"] = None
 		self._reset_ids()
 
 
+
+
+
+		
+
+
+
+
+
+
+
+	######## Methods for looking at or summarizing the whole dataset  #########
 
 
 
@@ -278,12 +352,9 @@ class Dataset:
 		self._reset_ids()
 
 
-
 	def write_to_csv(self, path):
 		self.sort_by_species()
 		self.df.to_csv(path, index=False)
-
-
 
 
 	def describe(self):
@@ -292,6 +363,9 @@ class Dataset:
 		print("Number of unique descriptions:   {}".format(len(pd.unique(self.df.description))))
 		print("Number of unique gene name sets: {}".format(len(pd.unique(self.df.gene_names))))
 		print("Number of species represented:   {}".format(len(pd.unique(self.df.species))))
+
+
+
 
 
 
