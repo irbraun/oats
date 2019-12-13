b31f6d6fd7a8e1df18f605b3aa0a5ac8083845ab
irbraun <irbraun@iastate.edu>
2019-12-13 17:05:12 -0600

    small changes

--- oats/graphs/pwgraph.py
+++ oats/graphs/pwgraph.py
@@ -4,7 +4,7 @@
 
 class PairwiseGraph:
 
-	def __init__(self, edgelist, vector_dictionary=None, vectorizer_object=None):
+	def __init__(self, edgelist, vector_dictionary=None, vectorizer_object=None, id_to_index=None, index_to_id=None, array=None):
 		"""
 		This class is for creating objects to hold the specifications for a pairwise
 		similarity or distance graph between a set of objects with IDs (such as genes
@@ -16,7 +16,14 @@ class PairwiseGraph:
 		    edgelist (pandas.DataFrame): Each row is an edge in the graph with format {from,to,value}.
 		    vector_dictionary (None, optional): A mapping between node IDs and vector representation.
 		    vectorizer_object (None, optional): The vectorizer (scipy) object used for embedding each node.
+		    id_to_index (None, optional): A mapping between ID integers and indices in the array.
+		    index_to_id (None, optional): A mapping between indices in the array and ID integers.
+		    array (None, optional): A numpy array where the 
+		
 		"""
 		self.edgelist = edgelist
 		self.vector_dictionary = vector_dictionary
 		self.vectorizer_object = vectorizer_object
+		self.id_to_index = id_to_index
+		self.index_to_id = index_to_id
+		self.array = array
