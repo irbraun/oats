b026fce60895f1d8b1e7fdb631d0c18ddbd1f0c4
irbraun <irbraun@iastate.edu>
2020-03-10 11:02:29 -0500

    updating documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # autodoc.
 
 from mock import Mock as MagicMock
- class Mock(MagicMock):
+class Mock(MagicMock):
 	@classmethod
 	def __getattr__(cls, name):
 		return MagicMock()
